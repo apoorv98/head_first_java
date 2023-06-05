2f33b4d00646a45a2804355144334e1846f2379b
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2023-06-05 12:52:48 +0530

    completing the action listeners

--- QuizCardBuilder.java
+++ QuizCardBuilder.java
@@ -71,16 +71,49 @@ public class QuizCardBuilder {
   }
 
   private class NextCardListener implements ActionListener {
-    public void actionPerformed(ActionEvent ev) {}
+    public void actionPerformed(ActionEvent ev) {
+      QuizCard card = new QuizCard(question.getText(), answer.getText());
+      cardList.add(card);
+      clearCard();
+    }
   }
 
   private class SaveMenuListener implements ActionListener {
-    public void actionPerformed(ActionEvent ev) {}
+    public void actionPerformed(ActionEvent ev) {
+      QuizCard card = new QuizCard(question.getText(), answer.getText());
+      cardList.add(card);
+
+      JFileChooser fileSave = new JFileChooser();
+      fileSave.showSaveDialog(frame);
+      saveFile(fileSave.getSelectedFile());
+    }
   }
 
   private class NewMenuListener implements ActionListener {
-    public void actionPerformed(ActionEvent ev) {}
+    public void actionPerformed(ActionEvent ev) {
+      cardList.clear();
+      clearCard();
+    }
   }
 
-  private void saveFile(File file) {}
+  private void clearCard() {
+    question.setText("");
+    answer.setText("");
+    question.requestFocus();
+  }
+
+  private void saveFile(File file) {
+    try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+      for (QuizCard card : cardList) {
+        writer.write(card.getQuestion() + "/");
+        writer.write(card.getAnswer() + "\n");
+      }
+      writer.close();
+    } catch (IOException ex) {
+      System.out.println("couldn't write the cardList out");
+      ex.printStackTrace();
+    }
+  }
 }
