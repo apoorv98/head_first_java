b88f9bc8dd2dce08a3d197f1a11377ca49c407b1
Apoorv Jagtap <apoorvjagtap@yahoo.com>
2023-06-05 12:12:21 +0530

    skeleton for quiz builder

---
+++ QuizCardBuilder.java
@@ -0,0 +1,17 @@
+public class QuizCardBuilder {
+  public void go() {}
+
+  private class NextCardListener implements ActionListener {
+    public void actionPerformed(ActionEvent ev) {}
+  }
+
+  private class SaveMenuListener implements ActionListener {
+    public void actionPerformed(ActionEvent ev) {}
+  }
+
+  private class NewMenuListener implements ActionListener {
+    public void actionPerformed(ActionEvent ev) {}
+  }
+
+  private void saveFile(File file) {}
+}
